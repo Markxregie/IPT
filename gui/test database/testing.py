4f3d90a1fea47ee0d163c7d79139d9e5f7732a94
Mark Regie A. Magtangob <marklegend029@gmail.com>
2024-06-09 22:56:59 +0800

    test database

---
+++ gui/test database/testing.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+try:
+    connect = sqlite3.connect('test.db')
+    
+    cursor = connect.cursor()
+    print ("The Database is Successfully Connected!")
+
+    strSQl = "select Sqlite_version();"
+    cursor.execute(strSQl)
+
+    record = cursor.fetchall()
+    print("SQlite Database Version is: ", record)
+
+    cursor.close()
+
+except sqlite3.Error as error:
+    print("Error Qhile connecting to sqlite0", error)
+
+finally:
+    if connect:
+        connect.close()
+        print("The SQlite connection is closed")
